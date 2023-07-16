82e105fe1ed5f307e9de8fa9efda76323aceccee
BNenzo <enzoletona06@gmail.com>
2023-07-16 20:30:32 -0300

    add Restauran list component

---
+++ components/RestaurantList.js
@@ -0,0 +1,29 @@
+import { ScrollView, StyleSheet, View } from "react-native";
+import RestaurantSection from "./RestaurantSection";
+
+const RestaurantList = ({ results }) => {
+  const filterResults = (priceFilter) => {
+    return results?.filter((result) => {
+      return result.price == priceFilter;
+    });
+  };
+
+  return (
+    <ScrollView style={styles.container}>
+      <RestaurantSection
+        title={"Cost Effective"}
+        results={filterResults("$")}
+      />
+      <RestaurantSection title={"Bit Pricier"} results={filterResults("$$")} />
+      <RestaurantSection title={"Big Spender"} results={filterResults("$$$")} />
+    </ScrollView>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    rowGap: 16,
+  },
+});
+
+export default RestaurantList;
