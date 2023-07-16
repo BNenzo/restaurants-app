e3e9a2de84a594f5956b81b2f23e91cef02d7ef4
BNenzo <enzoletona06@gmail.com>
2023-07-16 20:28:18 -0300

    add theme styles

---
+++ styles/theme.js
@@ -0,0 +1,17 @@
+export const theme = {
+  fontSizes: {
+    xSmall: 8,
+    small: 12,
+    medium: 16,
+    large: 24,
+    extraLarge: 32,
+  },
+  fontWeights: {
+    normal: "400",
+    bold: "800",
+  },
+  colors: {
+    primary: "black",
+    secondary: "grey",
+  },
+};
