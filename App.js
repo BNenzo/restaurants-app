f0e7ecb02ab5b52d0ccf1266b5792383cbbf1b45
BNenzo <enzoletona06@gmail.com>
2023-07-17 21:46:37 -0300

    remove unused components

--- App.js
+++ App.js
@@ -3,6 +3,7 @@ import { StyleSheet, Text, View } from "react-native";
 import { NavigationContainer } from "@react-navigation/native";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import HomeScreen from "./screens/HomeScreen";
+import ResultDetailScreen from "./screens/ResultDetailScreen";
 
 const Stack = createNativeStackNavigator();
 
@@ -19,6 +20,7 @@ export default function App() {
         }}
       >
         <Stack.Screen name="Business Search" component={HomeScreen} />
+        <Stack.Screen name="Details" component={ResultDetailScreen} />
       </Stack.Navigator>
     </NavigationContainer>
   );
--- components/RestaurantList.js
+++ components/RestaurantList.js
@@ -1,4 +1,4 @@
-import { ScrollView, StyleSheet, View } from "react-native";
+import { ScrollView, StyleSheet } from "react-native";
 import RestaurantSection from "./RestaurantSection";
 
 const RestaurantList = ({ results }) => {
